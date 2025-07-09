97a211be0ee37417aee4eaaccdf36f2d27f5afca
Bilaaef524 <bila45519@gmail.com>
2025-07-10 00:47:35 +0700

    Update DataHR Analytics_Kel.py

--- DataHR Analytics_Kel.py
+++ DataHR Analytics_Kel.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyN+amdbYcdiPsYl7a+F9Dvx",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
